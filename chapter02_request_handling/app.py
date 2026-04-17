016dfe4d39628aba31474acdf7e20d739479ccd0
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-04-17 21:18:46 +0530

    Day 04: POST requests, JSON handling, basic validation

---
+++ chapter02_request_handling/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import Config
+from routes.user_routes import user_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    app.register_blueprint(user_bp)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
