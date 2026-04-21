e30933b86a2d5f6ad8b88ec0d8d67045cf97ca20
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-04-21 19:52:37 +0530

    Day 08: Environment config, dotenv, dev/prod setup

--- chapter03_project_structure/app/__init__.py
+++ chapter03_project_structure/app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from config import Config
+from dotenv import load_dotenv
+import os
+
+from config import DevelopmentConfig, ProductionConfig
 
 from app.routes.user_routes import user_bp
 from app.routes.health_routes import health_bp
 
+load_dotenv()
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
 
-    # Register Blueprints
+    env = os.getenv("FLASK_ENV")
+
+    if env == "production":
+        app.config.from_object(ProductionConfig)
+    else:
+        app.config.from_object(DevelopmentConfig)
+
     app.register_blueprint(user_bp)
     app.register_blueprint(health_bp)
 
--- chapter03_project_structure/config.py
+++ chapter03_project_structure/config.py
@@ -1,3 +1,14 @@
+import os
+
 class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecret")
+    APP_NAME = os.getenv("APP_NAME", "Flask App")
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
     DEBUG = True
-    APP_NAME = "Flask Backend Mastery"
+
+
+class ProductionConfig(Config):
+    DEBUG = False
