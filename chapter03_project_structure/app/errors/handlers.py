d420d3d217e80ed6c1715ca2175c55392adf1933
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-04-22 19:38:38 +0530

    Day 09: Global error handling, logging, middleware hooks

---
+++ chapter03_project_structure/app/errors/handlers.py
@@ -0,0 +1,17 @@
+from flask import jsonify
+
+def register_error_handlers(app):
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": "Route not found"
+        }), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        return jsonify({
+            "success": False,
+            "error": "Internal server error"
+        }), 500
