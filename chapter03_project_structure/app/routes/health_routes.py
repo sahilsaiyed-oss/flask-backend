e4dbc2a8b7a51d064ce9b686a3167cbd351eaac8
sahilsaiyed-oss <sahilsaiyed067@gmail.com>
2026-04-22 19:39:40 +0530

    Day 09: Global error handling, logging, middleware hooks

--- chapter03_project_structure/app/routes/health_routes.py
+++ chapter03_project_structure/app/routes/health_routes.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 from app.utils.response import success_response
 
 health_bp = Blueprint("health", __name__)
 
 @health_bp.route("/health", methods=["GET"])
 def health():
-    return success_response("Service running", {"status": "OK"})
+    return success_response(
+        "Service running",
+        {
+            "status": "OK",
+            "app": current_app.config.get("APP_NAME")
+        }
+    )
