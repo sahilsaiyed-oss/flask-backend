27580507c8f57531027b4f93a258f207322dd2fe
Your GitHub Name <sahilsaiyed067@gmail.com>
2026-04-30 22:00:27 +0530

    Day 16: Implemented full CRUD with update and delete operations

--- chapter05_crud_database/app/routes/user_routes.py
+++ chapter05_crud_database/app/routes/user_routes.py
@@ -4,6 +4,7 @@ from app.models import User
 
 user_bp = Blueprint("user", __name__)
 
+
 @user_bp.route("/")
 def home():
     users = User.query.all()
@@ -18,6 +19,34 @@ def add_user():
     new_user = User(name=name, email=email)
 
     db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/")
+
+
+@user_bp.route("/edit-user/<int:user_id>")
+def edit_user_page(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("edit_user.html", user=user)
+
+
+@user_bp.route("/update-user/<int:user_id>", methods=["POST"])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+
+    user.name = request.form.get("name")
+    user.email = request.form.get("email")
+
+    db.session.commit()
+
+    return redirect("/")
+
+
+@user_bp.route("/delete-user/<int:user_id>")
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+
+    db.session.delete(user)
     db.session.commit()
 
     return redirect("/")
