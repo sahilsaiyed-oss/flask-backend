ba6da456c077d8354edfce08c892fb83712cc186
Your GitHub Name <sahilsaiyed067@gmail.com>
2026-05-04 21:52:42 +0530

    Day 18: Added search and filtering functionality to CRUD dashboard

--- chapter05_crud_database/app/routes/user_routes.py
+++ chapter05_crud_database/app/routes/user_routes.py
@@ -7,8 +7,21 @@ user_bp = Blueprint("user", __name__)
 
 @user_bp.route("/")
 def home():
-    users = User.query.all()
-    return render_template("users.html", users=users)
+    search_query = request.args.get("search", "")
+
+    if search_query:
+        users = User.query.filter(
+            (User.name.ilike(f"%{search_query}%")) |
+            (User.email.ilike(f"%{search_query}%"))
+        ).all()
+    else:
+        users = User.query.all()
+
+    return render_template(
+        "users.html",
+        users=users,
+        search_query=search_query
+    )
 
 
 @user_bp.route("/add-user", methods=["POST"])
@@ -28,45 +41,4 @@ def add_user():
     db.session.commit()
 
     flash("User added successfully!", "success")
-    return redirect("/")
-
-
-@user_bp.route("/edit-user/<int:user_id>")
-def edit_user_page(user_id):
-    user = User.query.get_or_404(user_id)
-    return render_template("edit_user.html", user=user)
-
-
-@user_bp.route("/update-user/<int:user_id>", methods=["POST"])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-
-    new_email = request.form.get("email")
-
-    duplicate_user = User.query.filter(
-        User.email == new_email,
-        User.id != user_id
-    ).first()
-
-    if duplicate_user:
-        flash("Another user already uses this email!", "error")
-        return redirect(f"/edit-user/{user_id}")
-
-    user.name = request.form.get("name")
-    user.email = new_email
-
-    db.session.commit()
-
-    flash("User updated successfully!", "success")
-    return redirect("/")
-
-
-@user_bp.route("/delete-user/<int:user_id>")
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-
-    db.session.delete(user)
-    db.session.commit()
-
-    flash("User deleted successfully!", "success")
     return redirect("/")
